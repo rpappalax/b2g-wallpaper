f78555722f321dc5a8fa5c04a4fa1755c00d825a
Richard Pappalardo <rpappalax@gmail.com>
2017-10-10 10:18:11 -0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# curl -X POST http://admin:mozSvc@qa.stage.mozaws.net:8080/computer/dummy_node2/config.xml --data-binary "@config.xml"
+# curl -X POST http://<host>:8080/computer/dummy_node2/config.xml --data-binary "@config.xml"
 
 #  adb shell screenshot /sdcard/screenshot.png
 #  adb pull /sdcard/screenshot.png
